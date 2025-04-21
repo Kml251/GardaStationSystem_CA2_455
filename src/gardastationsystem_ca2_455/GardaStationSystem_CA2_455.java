c6f83ebe0f6d7d56ba051de3f12bd8807895121c
Kml251 <2024455@student.cct.ie>
2025-04-21 22:02:05 +0100

    Creating CA_2 Project.

---
+++ src/gardastationsystem_ca2_455/GardaStationSystem_CA2_455.java
@@ -0,0 +1,21 @@
+/*
+ * CA_2 Project - Garda Station System
+ * Student Number: 2024455
+ * Student Name: Kamil Yildiz
+ */
+package gardastationsystem_ca2_455;
+
+/**
+ *
+ * @author kamil
+ */
+public class GardaStationSystem_CA2_455 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
