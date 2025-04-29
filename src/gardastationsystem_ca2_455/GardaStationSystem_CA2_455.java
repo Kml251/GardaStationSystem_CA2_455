11d2523a93448a66a4854e14fc67f2cd34d3ca05
Kml251 <2024455@student.cct.ie>
2025-04-29 02:04:47 +0100

    Added Garda and MyArrayList for sorting and listing the values.

--- src/gardastationsystem_ca2_455/GardaStationSystem_CA2_455.java
+++ src/gardastationsystem_ca2_455/GardaStationSystem_CA2_455.java
@@ -13,23 +13,28 @@
  * - Exit the application
  * 
  * Student ID: 2024455
- * Author: kamil
+ * @author kamil
  */
-
 package gardastationsystem_ca2_455;
 
 import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Random;
 
-/**
- *
- * @author kamil
- */
+
 public class GardaStationSystem_CA2_455 {
 
     /**
      * @param args the command line arguments
+     * @throws java.io.FileNotFoundException
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        MyArrayList<Garda> gardaList = new MyArrayList<>(); // Create Garda list
+        readNamesFromFile(gardaList);                   // Load Garda names from file
+        
         // Use try-with-resources to ensure Scanner is closed automatically
         try (Scanner kb = new Scanner(System.in)) {
             boolean exit = false; // Controls when to exit the main menu loop
@@ -62,11 +67,35 @@ public class GardaStationSystem_CA2_455 {
 
                 // Execute the selected menu option
                 switch (option) {
-                    case SORT -> System.out.println("-> Sorting Gardaí list...");
+                    case SORT -> {
+                        System.out.println("-> Sorting Gardaí list...");
+                        gardaList.bubbleRecursiveSort();
+                        
+                        System.out.println("\n=== First 20 Sorted Gardaí ===");
+                        System.out.printf("%-8s %-40s %-30s %-30s%n", "#", "Name", "Manager", "Department");
+                        System.out.println("------------------------------------------------------------------------------------------------------");
+                        
+                        for(int i=0; i < Math.min(20, gardaList.size()); i++){
+                            System.out.println((i+1) +". \t" + gardaList.get(i));
+                        }
+                    }
                     case SEARCH -> System.out.println("-> Searching records...");
                     case ADD_RECORD -> System.out.println("-> Adding a new Garda...");
                     case GENERATE_RANDOM -> System.out.println("-> Generating random Gardaí...");
-                    case DISPLAY_ALL -> System.out.println("-> Displaying all Gardaí...");
+                    case DISPLAY_ALL -> {
+                        System.out.println("-> Displaying all Gardaí...");
+                        if (gardaList.isEmpty()){
+                            System.out.println("No Gardaí found.");
+                        }
+                        else{
+                            System.out.printf("%-8s %-40s %-30s %-30s%n", "#", "Name", "Manager", "Department");
+                            System.out.println("------------------------------------------------------------------------------------------------------");
+                            
+                            for(int i=0; i < gardaList.size(); i++){
+                                System.out.println((i+1) + ".\t" + gardaList.get(i));
+                            }
+                        }
+                    }
                     case DELETE_GARDA -> System.out.println("-> Deleting a Garda...");
                     case EXPORT_REPORT -> System.out.println("-> Exporting report...");
                     case EXIT -> {
@@ -80,6 +109,41 @@ public class GardaStationSystem_CA2_455 {
             // Final message after exiting
             System.out.println("\nProgram finished. Goodbye!");
         }
+    }
+
+    private static void readNamesFromFile(MyArrayList<Garda> gardaList) throws FileNotFoundException {
+        
+        String filename = "Applicants_Form.txt"; // To read txt file
+        
+        String[] managerTypes = {"Garda Commissioner",
+                                 "Deputy Commissioner",
+                                 "Superintendent",
+                                 "Inspector",
+                                 "Sergeant"};
+        String[] departments = {"Crime Investigaion Unit",
+                                 "Cyber Crime Unit",
+                                 "Drugs & Organised Crime Bureau",
+                                 "Community Policing Unit",
+                                 "National Immigration Bureau"};
+        
+        Random random = new Random(); // To split the types and departments randomly
+        BufferedReader reader = new BufferedReader(new FileReader(filename)); 
+        try{
+            String line;
+            
+            while ((line = reader.readLine()) != null){
+            
+            if(!line.trim().isEmpty()){
+                String randomManager = managerTypes[random.nextInt(managerTypes.length)];
+                    String randomDepartment = departments[random.nextInt(departments.length)];
+                    Garda newGarda = new Garda(line.trim(), randomManager, randomDepartment);
+                    gardaList.add(newGarda);
+            }
+            System.out.println("File read successfully. Gardaí loaded from Applicants_Form.txt.\n");
+            } 
+        }catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }   
     }
     
 }
