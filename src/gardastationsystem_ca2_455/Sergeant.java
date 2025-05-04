26d26fb439ea8793b1c449c9080162fec553fb3c
Kml251 <2024455@student.cct.ie>
2025-05-04 22:19:42 +0100

    Added Sergeant subclass.

---
+++ src/gardastationsystem_ca2_455/Sergeant.java
@@ -0,0 +1,14 @@
+package gardastationsystem_ca2_455;
+
+/**
+ * Represents the Sergeant role.
+ * @author kamil
+ */
+public class Sergeant extends Manager {
+    /*
+    * Constructs a Sergeant with the predefined title.
+    */
+    public Sergeant() {
+        super("Sergeant");
+    }
+}
