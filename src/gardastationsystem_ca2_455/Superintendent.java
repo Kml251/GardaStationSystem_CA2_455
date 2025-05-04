cd3194f8d41877cf229fd1635aa7943a0e6b38c0
Kml251 <2024455@student.cct.ie>
2025-05-04 22:14:08 +0100

    Added Superintendent subclass.

---
+++ src/gardastationsystem_ca2_455/Superintendent.java
@@ -0,0 +1,15 @@
+package gardastationsystem_ca2_455;
+
+/**
+ * Represents the Superintendent role.
+ * @author kamil
+ */
+public class Superintendent extends Manager {
+    /*
+    * Constructs a Superintendent with the predefined title.
+    */
+    public Superintendent() {
+        super("Superintendent");
+    }
+    
+}
