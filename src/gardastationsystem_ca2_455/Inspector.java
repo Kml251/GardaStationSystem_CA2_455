4125f74327750e418aed3f92962bdc3869cbfad3
Kml251 <2024455@student.cct.ie>
2025-05-04 22:16:40 +0100

    Added Inspector subclass.

---
+++ src/gardastationsystem_ca2_455/Inspector.java
@@ -0,0 +1,15 @@
+package gardastationsystem_ca2_455;
+
+/**
+ * Represents the Inspector role in the Garda System.
+ * @author kamil
+ */
+public class Inspector extends Manager{
+    /*
+    * Constructs an Inspector with the predefined title.
+    */
+    public Inspector() {
+        super("Inspector");
+    }
+    
+}
