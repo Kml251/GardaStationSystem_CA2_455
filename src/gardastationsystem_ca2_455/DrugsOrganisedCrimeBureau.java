7bb41900c68fadb3a00d146635f0708f65cb22fe
Kml251 <2024455@student.cct.ie>
2025-05-05 00:29:05 +0100

    Added DrugsOrganisedCrimeBureau subclass.

---
+++ src/gardastationsystem_ca2_455/DrugsOrganisedCrimeBureau.java
@@ -0,0 +1,16 @@
+package gardastationsystem_ca2_455;
+
+/**
+ * Represents the DrugsOrganisedCrimeBureau,
+ * handling drug trafficking and major criminal networks.
+ * @author kamil
+ */
+public class DrugsOrganisedCrimeBureau extends Department {
+    /*
+    * Constructs a DrugsOrganisedCrimeBureau department.
+    */
+    public DrugsOrganisedCrimeBureau() {
+        super("Drugs Organised Crime Bureau");
+    }
+    
+}
