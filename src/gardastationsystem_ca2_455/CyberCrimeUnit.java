31ac9ffe70409085542a2bd1da31a9a870103b6c
Kml251 <2024455@student.cct.ie>
2025-05-05 00:17:57 +0100

    Added CyberCrimeUnit subclass.

---
+++ src/gardastationsystem_ca2_455/CyberCrimeUnit.java
@@ -0,0 +1,15 @@
+package gardastationsystem_ca2_455;
+
+/**
+ * Represents the CyberCrimeUnit department,
+ * dealing with digital forensics, hacking, and cyber threats.
+ * @author kamil
+ */
+public class CyberCrimeUnit extends Department{
+    /*
+    * Constructs a CyberCrimeUnit department.
+    */
+    public CyberCrimeUnit() {
+        super ("Cyber Crime Unit");
+    }    
+}
