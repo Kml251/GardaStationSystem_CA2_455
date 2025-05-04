fb3879a3cf6361b927b862c480c75a61b24caf41
Kml251 <2024455@student.cct.ie>
2025-05-04 22:10:46 +0100

    Added DeputyCommissioner subclass.

---
+++ src/gardastationsystem_ca2_455/DeputyCommissioner.java
@@ -0,0 +1,15 @@
+package gardastationsystem_ca2_455;
+
+/**
+ * Represents the Deputy Commissioner role.
+ * @author kamil
+ */
+public class DeputyCommissioner extends Manager {
+    /*
+    * Constructs a Deputy Commissioner with the predefined title.
+    */
+    public DeputyCommissioner() {
+        super ("Deputy Commissioner");
+    }
+    
+}
