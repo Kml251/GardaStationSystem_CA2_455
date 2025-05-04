1001dd9930a1a05aee86ded3b3de0670784afb81
Kml251 <2024455@student.cct.ie>
2025-05-05 00:34:06 +0100

    Added NationalImmigrationBureau subclass.

---
+++ src/gardastationsystem_ca2_455/NationalImmigrationBureau.java
@@ -0,0 +1,16 @@
+package gardastationsystem_ca2_455;
+
+/**
+ * Represents the NationalImmigrationBureau,
+ * handling immigration enforcement and investigations.
+ * @author kamil
+ */
+public class NationalImmigrationBureau extends Department {
+    /*
+    * Construct a NationalImmigrationBureau department.
+    */
+    public NationalImmigrationBureau() {
+        super("National Immigration Bureau");
+    }
+    
+}
