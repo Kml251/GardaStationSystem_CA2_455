f1d746a3be9f08546b872d5fb1752a24b062bbd8
Kml251 <2024455@student.cct.ie>
2025-05-05 00:21:41 +0100

    Added CrimeInvestigationUnit subclass.

---
+++ src/gardastationsystem_ca2_455/CrimeInvestigationUnit.java
@@ -0,0 +1,15 @@
+package gardastationsystem_ca2_455;
+
+/**
+ * Represents the CrimeInvestigationUnit,
+ * focusing on serious and organised crime.
+ * @author kamil
+ */
+public class CrimeInvestigationUnit extends Department {
+    /*
+    * Constructs a CrimeInvestigationUnit department.
+    */
+    public CrimeInvestigationUnit(){
+        super("Crime Investigation Unit");
+    }
+}
