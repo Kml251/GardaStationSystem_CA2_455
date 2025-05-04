cacd2f1a4deb1c9f7a82c51e1498471d0db82605
Kml251 <2024455@student.cct.ie>
2025-05-05 00:25:48 +0100

    Added CommunityPolicingUnit subclass.

---
+++ src/gardastationsystem_ca2_455/CommunityPolicingUnit.java
@@ -0,0 +1,16 @@
+package gardastationsystem_ca2_455;
+
+/**
+ * Represents the CommunityPolicingUnit,
+ * working closely with local neighborhoods and public safety.
+ * @author kamil
+ */
+public class CommunityPolicingUnit extends Department{
+    /*
+    * Constructs a CommunityPolicingUnit department.
+    */
+    public CommunityPolicingUnit() {
+        super("Community Policing Unit");
+    }
+    
+}
